69935dcaac04a6adf12aab2aff08e002a11617d1
Sean Clark <sean@square-bracket.com>
2012-10-31 21:41:00 -0400

    took out anonymous function in ajax file

--- ajax.php
+++ ajax.php
@@ -17,20 +17,25 @@
 		"fields"=>array(
 			"thumb" => "CONCAT('http://cmivfx.com/images/thumbs/',ThumbnailLocation)"
 		),
-		"select" => function($grid) {
-			$selects = array();
-		
-			// category select
-			$grid->table = "categories";
-			$selects["CategoryID"] = $grid->makeSelect("CategoryID","Name");
-			
-			// active select
-			$selects["active"] = array("1"=>"true","0"=>"false");
-			
-			// render data			
-			$grid->render($selects);
-		}
+		"select" => 'selectFunction'
 	));
+	
+	// drop down function
+	// if you have anonymous function support, then you can just put this function in place of
+	// 'selectFunction'
+	function selectFunction($grid) {
+		$selects = array();
+	
+		// category select
+		$grid->table = "categories";
+		$selects["CategoryID"] = $grid->makeSelect("CategoryID","Name");
+		
+		// active select
+		$selects["active"] = array("1"=>"true","0"=>"false");
+		
+		// render data			
+		$grid->render($selects);
+	}
 
 	
 ?>
